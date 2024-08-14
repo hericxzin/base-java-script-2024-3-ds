59289c22ba7564a7e2aa00dc99c0f5dab331d78c
hericxzin <140723994+hericxzin@users.noreply.github.com>
2024-08-14 10:38:06 -0300

    Add files via upload

---
+++ variáveis.js
@@ -0,0 +1,60 @@
+// variavéis
+// tipo number
+
+const meuNumero = 6;
+const NumeroUm = 7;
+const NumeroDois = 2;
+
+
+const soma = NumeroDois + meuNumero
+console.log(soma)
+
+const subtracao = NumeroUm - meuNumero;
+console.log(subtracao);
+
+const multiplicacao = NumeroDois = 10;
+console.log(multiplicacao);
+
+const divisao = meuNumero / NumeroDois
+console.log(divisao)
+
+// float (numero decimal)
+
+const numeroTres = 3.3;
+const numeroQuatro = .5  // mesma coisa que 0.5
+
+const operacaom = numeroTres * numeroQuatro
+console.log(operacaoM)
+
+// NaN A Number 
+
+const operacaoM = "a" * meuNumero
+console.log(operacaom)
+
+const novaOperacao = "a" = meuNumero
+console.log(novaOperacao)
+
+// string (texto)
+
+const meuNome = "heric"
+const frase = "Meu nome é" 
+const issoNaoEUmNumero = '50'
+
+const minhaFrase = frase + meuNome;
+console.log(minhaFrase)
+
+const operacaoO = issoNaoEUmNumero = meuNumero
+console.log(operacaoM);
+
+//boolean
+//True -> verdadeiro;
+//False -> falso;
+
+const comparacao = 6 === meuNumero;
+console.log(comparacao)
+
+const comparacaoII = "6" === meuNumero;
+console.log(comparacaoII);
+
+const comparacaoIII = "6" == meuNumero;
+console.log(comparacaoIII);
